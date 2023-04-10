13969b8a6f5c44cc6f04220e2cb2bbc74f00124d
gourcetools <120996278+gourcetools@users.noreply.github.com>
2023-04-10 18:27:00 +0200

    Add repo name to published note | Remove emojis stuff | Clean script | Use relay.damus.io

--- check_commits.sh
+++ check_commits.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 # Configuration
-PRIVKEY=<PRIVEY IN HEX FORMAT>
+PRIVKEY="PUT A HEX PRIVKEY HERE"
 RELAY="wss://nostr.mutinywallet.com"
 RELAYBIS="wss://relay.nostr.band"
-RELAYFALLBACK="wss://relay.nostriches.org"
+RELAYFALLBACK="wss://relay.damus.io"
 POW=10
 ASKINTERVAL=30
 
 
-# List of emojis
-
-emoji_list=("🎉" "🎊" "🥳" "🍾" "🎈" "🎇" "🙌" "🎉" "🎊" "🥳" "🌻" "🌼" "🌸" "🙌" "🌺" "🌹" "🌷" "😊" "😃" "😄" "😍" "😘" "😎" "😜" "😛" "😇" "🙂" "🤗" "🤩" "🥰" "😊")
-
 
 while true; do
-	# Use "shuf" to randomly select an emoji
-	random_emoji=$(printf "%s\n" "${emoji_list[@]}" | shuf -n 1)
-	
+
 	# Save NOTE to REPOURLOLD variable before checking note, so 
 	# we can compare them later to see if there is a new event id.
  	REPOURLOLD=$REPOURLNEW
- 	
-	
 	cp ./COMMIT-ID.txt ./OLD-ID.txt
-	sleep 3
-	echo " == ❓Asking github for new commits..."
+	echo " ==  Running check-commits.js ..."
 	echo " == ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ="
 	# NODE node.js for new commits 
 	node ./check_commits.js 
-		echo " == ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ⌛ ="
+		echo " == Done.  ="
 	# Set the outputs of crawl.js as id and pubkey
 	NAME=$(cat ./COMMIT-NAME.txt)
 	REPOTITLE=$(cat ./REPO-TITLE.txt)
@@ -36,24 +27,23 @@ while true; do
 	ID=$(cat ./COMMIT-ID.txt)
 	OLDID=$(cat ./OLD-ID.txt)
 	echo " "
-	echo "name:	$NAME "
-	echo "url:	$URL "
-	echo "id	$ID "
-	echo "old-id:	$OLDID "	
+	echo "repo:	$REPOTITLE "
+	echo "commit:	$URL "
+	
 	echo " "
 		if [ "$OLDID" == "$ID" ]; then   	
-        	echo " == ❌ No new commits... checking again in $ASKINTERVAL seconds"
+        	echo " == ❌ Nothing new... checking again in $ASKINTERVAL seconds"
 		else
-		echo " == 🆕 ✅ New commits found, let's publish!"
+		echo " == 🆕 ✅ New stuff found, let's publish about it!"
 		# Reply to message using nostril
 
-		nostril --envelope --pow "$POW" --sec "$PRIVKEY" --content "$random_emoji $NAME
+		nostril --envelope --pow "$POW" --sec "$PRIVKEY" --content "$REPOTITLE : $NAME
 $URL " | tee >(websocat $RELAY) >(websocat $RELAYBIS) >(websocat $RELAYFALLBACK)
 		
 
-		echo " == OK . EVENT ID ABOVE.  RELAY: "$RELAY" "
+		echo " == OK . EVENT ID ABOVE. "
 		echo " "
-		echo " == ⌛Checking for new commits in $ASKINTERVAL seconds..."
+		echo " == ⌛Checking again in $ASKINTERVAL seconds..."
 		echo " "
 	fi
 sleep $ASKINTERVAL
